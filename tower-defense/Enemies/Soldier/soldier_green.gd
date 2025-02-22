4bcfef42345ea7a172149b6e92f6007ef5ef6813
Prabhudeba <137163103+prabhudeba1111@users.noreply.github.com>
2025-02-22 20:59:55 +0530

    added path

---
+++ tower-defense/Enemies/Soldier/soldier_green.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+var speed :int= 100
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	get_parent().set_progress(get_parent().get_progress() + speed * delta)
