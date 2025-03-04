8462ab1c72cd8a0e63e4a78221cf3bb4dcdba6d2
prabhudeba.nath <prabhudeba.nath@intellectdesign.com>
2025-03-04 17:23:45 +0530

    main menu and level select

---
+++ tower-defense/UI/main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_new_game_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_settings_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_game_button_pressed() -> void:
+	get_tree().quit()
